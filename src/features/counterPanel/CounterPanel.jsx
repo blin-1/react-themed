25c20f990591890dbda4dda507f9675affe95e44
blin-1 <y.monsoon@gmail.com>
2020-04-04 18:19:48 -0400

    Animation

--- src/features/counterPanel/CounterPanel.jsx
+++ src/features/counterPanel/CounterPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import { Counter } from '../counter/Counter';
-import './CounterPanel.module.css';
+import './CounterPanel.css';
 
 function CounterPanel() {
   return (
--- src/features/tabPanel/TabPanel.jsx
+++ src/features/tabPanel/TabPanel.jsx
@@ -10,7 +10,7 @@ import Box from '@material-ui/core/Box';
 // Tab contents
 
 import LoginPanel    from '../loginPanel/LoginPanel'
-import CounterPanel from '../counterPanel/CounterPanel'  
+import CounterPanel from '../counterPanel/CounterPanel.jsx'  
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
