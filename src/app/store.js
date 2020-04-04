9da2cdb0787337cc1d05ba243c061c5325b84ebf
blin-1 <y.monsoon@gmail.com>
2020-04-04 18:25:03 -0400

    counter.slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from '../features/counter/Counter.slice';
 
 export default configureStore({
   reducer: {
